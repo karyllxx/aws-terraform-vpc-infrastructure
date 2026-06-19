bf24eb15d1c40a3066cb870177862a21ba947d18
Karina Miranda <kary0508miranda@gmail.com>
2026-06-19 17:37:36 -0500

    fix: remove duplicate resources and apply terraform fmt configuration

--- tf_code_1/resources.tf
+++ tf_code_1/resources.tf
@@ -24,55 +24,6 @@ resource "aws_internet_gateway" "cmtr-f1ae3864-igw" {
 }
 
 resource "aws_subnet" "cmtr-f1ae3864-public-subnet" {
-  vpc_id            = aws_vpc.cmtr-f1ae3864-vpc.id
-  cidr_block        = var.public_subnet_cidr
-
-  tags = {
-    Name = local.public_subnet_name
-  }
-}
-
-resource "aws_subnet" "cmtr-f1ae3864-private-subnet" {
-  vpc_id            = aws_vpc.cmtr-f1ae3864-vpc.id
-  cidr_block        = var.private_subnet_cidr
-
-  tags = {
-    Name = local.private_subnet_name
-  }
-}
-
-resource "aws_route_table" "cmtr-f1ae3864-public-rt" {
-  vpc_id = aws_vpc.cmtr-f1ae3864-vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.cmtr-f1ae3864-igw.id
-  }
-
-  tags = {
-    Name = local.public_rt_name
-  }
-}
-
-resource "aws_route_table" "cmtr-f1ae3864-private-rt" {
-  vpc_id = aws_vpc.cmtr-f1ae3864-vpc.id
-
-  tags = {
-    Name = local.private_rt_name
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.cmtr-f1ae3864-public-subnet.id
-  route_table_id = aws_route_table.cmtr-f1ae3864-public-rt.id
-}
-
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.cmtr-f1ae3864-private-subnet.id
-  route_table_id = aws_route_table.cmtr-f1ae3864-private-rt.id
-}
-
-resource "aws_subnet" "cmtr-f1ae3864-public-subnet" {
   vpc_id     = aws_vpc.cmtr-f1ae3864-vpc.id
   cidr_block = var.public_subnet_cidr
 
